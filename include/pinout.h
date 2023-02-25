4ddc10ac3a02dc9ce266282e2c06e959ab73e86d
Philipp Molitor <philipp@molitor-consulting.com>
2023-02-25 15:37:51 +0100

    add undocumented interrupt pins

--- include/pinout.h
+++ include/pinout.h
@@ -1,8 +1,10 @@
 #pragma once
 
-// QMI8658C IMU via I2C
-#define PIN_IMU_SDA 6 // IMU I2C data
-#define PIN_IMU_SCL 7 // IMU I2C clock
+// QMI8658C IMU via I2C on I2C1 / Wire1
+#define PIN_IMU_SDA 6    // IMU I2C data
+#define PIN_IMU_SCL 7    // IMU I2C clock
+#define PIN_IMU_INT_1 23 // IMU interrupt 1
+#define PIN_IMU_INT_2 24 // IMU interrupt 2
 
 // GC9A01 LCD via 4-Wire SPI
 #define PIN_LCD_DC 8    // command/data selection
