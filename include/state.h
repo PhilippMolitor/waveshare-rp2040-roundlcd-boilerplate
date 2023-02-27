5998ca436fc6d34c3ea1843f06f759424fb7c16e
Philipp Molitor <philipp@molitor-consulting.com>
2023-02-27 12:11:39 +0100

    use state struct for application state

---
+++ include/state.h
@@ -0,0 +1,24 @@
+#pragma once
+
+template <class T>
+struct vec3_t {
+  T x;
+  T y;
+  T z;
+};
+
+typedef struct imu_data_t {
+  bool ready;
+  float temp;
+  vec3_t<float> acc;
+  vec3_t<float> gyro;
+} imu_data_t;
+
+typedef struct battery_data_t {
+  float voltage;
+} battery_data_t;
+
+typedef struct state_t {
+  imu_data_t imu;
+  battery_data_t battery;
+} state_t;
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Wire.h>
 
 #include "pinout.h"
+#include "state.h"
 
 #include "battery.hpp"
 #include "lgfx_gc9a01.hpp"
@@ -10,11 +11,19 @@
 //
 // application state
 //
-static float state_battery_voltage = 0.0f;
-static boolean state_imu_initialized = false;
-static float state_imu_temp = 0.0f;
-static float state_imu_acc[3] = {0.0f, 0.0f, 0.0f};
-static float state_imu_gyro[3] = {0.0f, 0.0f, 0.0f};
+state_t state = {
+    .imu =
+        {
+            .ready = false,
+            .temp = 0.0f,
+            .acc = {.x = 0.0f, .y = 0.0f, .z = 0.0f},
+            .gyro = {.x = 0.0f, .y = 0.0f, .z = 0.0f},
+        },
+    .battery =
+        {
+            .voltage = 0.0f,
+        },
+};
 
 // battery
 static Battery battery;
@@ -25,15 +34,16 @@ static LGFX_GC9A01 display;
 
 void battery_tick() {
   battery.read();
-  state_battery_voltage = battery.get_voltage();
+  state.battery.voltage = battery.get_voltage();
 }
 
 void imu_tick() {
-  imu.read_temperature(&state_imu_temp);
-  imu.read_accelerometer(&state_imu_acc[0], &state_imu_acc[1],
-                         &state_imu_acc[2]);
-  imu.read_gyroscope(&state_imu_gyro[0], &state_imu_gyro[1],
-                     &state_imu_gyro[2]);
+  if (!state.imu.ready)
+    return;
+
+  imu.read_temperature(&state.imu.temp);
+  imu.read_accelerometer(&state.imu.acc.x, &state.imu.acc.y, &state.imu.acc.z);
+  imu.read_gyroscope(&state.imu.gyro.x, &state.imu.gyro.y, &state.imu.gyro.z);
 }
 
 void display_tick() {
@@ -41,22 +51,22 @@ void display_tick() {
 
   // voltage
   char voltage_str[10];
-  sprintf(voltage_str, "Bat: %.2f V", state_battery_voltage);
+  sprintf(voltage_str, "Bat: %.2f V", state.battery.voltage);
   display.drawRightString(voltage_str, 228, 70);
 
   // IMU temp
   char imu_temp_str[20];
-  sprintf(imu_temp_str, "IMU temp: %.2f C", state_imu_temp);
+  sprintf(imu_temp_str, "IMU temp: %.2f C", state.imu.temp);
   display.drawString(imu_temp_str, 12, 70);
   // IMU data
   char imu_ax_str[18], imu_ay_str[18], imu_az_str[18];
   char imu_gx_str[21], imu_gy_str[21], imu_gz_str[21];
-  sprintf(imu_ax_str, "ax: %.2f g", state_imu_acc[0]);
-  sprintf(imu_ay_str, "ay: %.2f g", state_imu_acc[1]);
-  sprintf(imu_az_str, "az: %.2f g", state_imu_acc[2]);
-  sprintf(imu_gx_str, "gx: %.2f dps", state_imu_gyro[0]);
-  sprintf(imu_gy_str, "gy: %.2f dps", state_imu_gyro[1]);
-  sprintf(imu_gz_str, "gz: %.2f dps", state_imu_gyro[2]);
+  sprintf(imu_ax_str, "ax: %.2f g", state.imu.acc.x);
+  sprintf(imu_ay_str, "ay: %.2f g", state.imu.acc.y);
+  sprintf(imu_az_str, "az: %.2f g", state.imu.acc.z);
+  sprintf(imu_gx_str, "gx: %.2f dps", state.imu.gyro.x);
+  sprintf(imu_gy_str, "gy: %.2f dps", state.imu.gyro.y);
+  sprintf(imu_gz_str, "gz: %.2f dps", state.imu.gyro.z);
   display.drawString(imu_ax_str, 12, 90);
   display.drawString(imu_ay_str, 12, 100);
   display.drawString(imu_az_str, 12, 110);
@@ -77,8 +87,8 @@ void setup() {
   Wire1.setSCL(PIN_IMU_SCL);
   Wire1.setClock(400'000);
   Wire1.begin();
-  state_imu_initialized = imu.begin(&Wire1, QMI8658C_I2C_ADDRESS_PULLUP);
-  if (state_imu_initialized) {
+  state.imu.ready = imu.begin(&Wire1, QMI8658C_I2C_ADDRESS_PULLUP);
+  if (state.imu.ready) {
     imu.configure_acc(QMI8658C::AccScale::ACC_SCALE_4G,
                       QMI8658C::AccODR::ACC_ODR_250HZ,
                       QMI8658C::AccLPF::ACC_LPF_5_32PCT);
