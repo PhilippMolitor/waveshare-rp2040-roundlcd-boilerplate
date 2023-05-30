5514e20520c662b4fdd283cd1fcd50d7a6f8b4ae
Philipp Molitor <philipp@molitor-consulting.com>
2023-05-30 13:36:50 +0200

    correctly set up typedefs

--- include/state.h
+++ include/state.h
@@ -12,26 +12,29 @@
     interrupts();                          \
   }
 
-template <class T>
-struct vec3_t {
+template <typename T>
+struct vec3 {
   T x;
   T y;
   T z;
 };
 
-typedef struct imu_data_t {
+template <typename T>
+using vec3_t = vec3<T>;
+
+typedef struct imu_data {
   bool ready;
   float temp;
   vec3_t<float> acc;
   vec3_t<float> gyro;
 } imu_data_t;
 
-typedef struct battery_data_t {
+typedef struct battery_data {
   float voltage;
   float percentage;
 } battery_data_t;
 
-typedef struct state_t {
+typedef struct state {
   uint32_t counter;
   imu_data_t imu;
   battery_data_t battery;
